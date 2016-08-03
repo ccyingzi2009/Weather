cfbb2256c4a2f8570c6b723ec8147fbe777ca85c
liu_shuai <liu_shuai@corp.netease.com>
2016-08-03 22:49:35 +0800

    首页修改

--- app1/src/main/java/netease/com/weather/ui/MainActivity.java
+++ app1/src/main/java/netease/com/weather/ui/MainActivity.java
@@ -1,70 +1,65 @@
 package netease.com.weather.ui;
 
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.ActionBar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
 
-import com.roughike.bottombar.BottomBar;
-import com.roughike.bottombar.BottomBarFragment;
-
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import netease.com.weather.R;
-import netease.com.weather.data.api.BYApi;
-import netease.com.weather.data.api.BYService;
-import netease.com.weather.data.model.TopTen;
 import netease.com.weather.ui.base.BaseActivity;
-import netease.com.weather.ui.biz.article.ArticleFragment;
-import netease.com.weather.ui.biz.custom.CustomViewFragment;
 import netease.com.weather.ui.biz.main.SampleFragment;
-import retrofit2.Call;
-import retrofit2.Callback;
 
 
 public class MainActivity extends BaseActivity {
 
-    private BottomBar mBottomBar;
+    @Bind(R.id.nav_view)
+    NavigationView navView;
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setElevation(0);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
+        if (fragment != null) {
+            if (fragment.isDetached()) {
+                ft.attach(fragment);
+            }
+        } else {
+            SampleFragment sampleFragment = SampleFragment.newInstance("");
+            ft.add(R.id.fragmentContainer, sampleFragment);
+            ft.commit();
+
         }
 
+        View headerView = navView.inflateHeaderView(R.layout.nav_header_main);
+        //todo headerView
 
-        mBottomBar = BottomBar.attach(this, savedInstanceState);
-        mBottomBar.setFragmentItems(getSupportFragmentManager(), R.id.fragmentContainer,
-                new BottomBarFragment(SampleFragment.newInstance(""), R.drawable.ic_recents, "Top10"),
-                new BottomBarFragment(ArticleFragment.newInstance(), R.drawable.ic_favorites, "Favorites"),
-                new BottomBarFragment(CustomViewFragment.newInstance(), R.drawable.ic_nearby, "Nearby"),
-                new BottomBarFragment(SampleFragment.newInstance("Content for friends."), R.drawable.ic_friends, "Friends"),
-                new BottomBarFragment(SampleFragment.newInstance("Content for food."), R.drawable.ic_restaurants, "Food")
-        );
-
-        mBottomBar.mapColorForTab(0, ContextCompat.getColor(this, R.color.colorAccent));
-        mBottomBar.mapColorForTab(1, 0xFF5D4037);
-        mBottomBar.mapColorForTab(2, "#7B1FA2");
-        mBottomBar.mapColorForTab(3, "#FF5252");
-        mBottomBar.mapColorForTab(4, "#FF9800");
-
-    }
-
-    private void initData() {
-        Call<TopTen> tops = BYApi.get().getApi().getTopTen(BYService.auth);
-        tops.enqueue(new Callback<TopTen>() {
-            @Override
-            public void onResponse(Call<TopTen> call, retrofit2.Response<TopTen> response) {
-                TopTen list = response.body();
-                System.out.println(list.toString());
-            }
-
-            @Override
-            public void onFailure(Call<TopTen> call, Throwable t) {
-                System.out.println("failure");
-            }
-        });
+//        mBottomBar = BottomBar.attach(this, savedInstanceState);
+//        mBottomBar.setFragmentItems(getSupportFragmentManager(), R.id.fragmentContainer,
+//                new BottomBarFragment(SampleFragment.newInstance(""), R.drawable.ic_recents, "Top10"),
+//                new BottomBarFragment(ArticleFragment.newInstance(), R.drawable.ic_favorites, "Favorites"),
+//                new BottomBarFragment(CustomViewFragment.newInstance(), R.drawable.ic_nearby, "Nearby"),
+//                new BottomBarFragment(SampleFragment.newInstance("Content for friends."), R.drawable.ic_friends, "Friends"),
+//                new BottomBarFragment(SampleFragment.newInstance("Content for food."), R.drawable.ic_restaurants, "Food")
+//        );
+//
+//        mBottomBar.mapColorForTab(0, ContextCompat.getColor(this, R.color.colorAccent));
+//        mBottomBar.mapColorForTab(1, 0xFF5D4037);
+//        mBottomBar.mapColorForTab(2, "#7B1FA2");
+//        mBottomBar.mapColorForTab(3, "#FF5252");
+//        mBottomBar.mapColorForTab(4, "#FF9800");
 
     }
+
 }

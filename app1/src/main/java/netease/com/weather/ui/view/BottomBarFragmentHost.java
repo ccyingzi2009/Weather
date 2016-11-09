8ffae7b6057d7792a93759449e9fca8220c31779
liu_shuai <liu_shuai@corp.netease.com>
2016-11-09 23:20:39 +0800

    bottombar资源替换

---
+++ app1/src/main/java/netease/com/weather/ui/view/BottomBarFragmentHost.java
@@ -0,0 +1,11 @@
+package netease.com.weather.ui.view;
+
+import com.roughike.bottombar.BottomBar;
+
+/**
+ * Created by liu_shuai on 2016/11/9.
+ */
+
+public class BottomBarFragmentHost {
+    private BottomBar mBottomBar;
+}
